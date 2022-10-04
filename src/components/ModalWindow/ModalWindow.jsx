d438e760babdff0443a4ab1e5b97df725d127770
Serhii Kudriavtsev <96264045+SerhiiKudriavtsev@users.noreply.github.com>
2022-10-04 21:34:13 +0300

    Update ModalWindow.jsx

--- src/components/ModalWindow/ModalWindow.jsx
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -1,41 +1,76 @@
-import { Component } from 'react';
-import propTypes from 'prop-types';
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { Overlay, ModalImage } from './ModalWindow.styled';
 
-class ModalWindow extends Component {
-  static propTypes = {
-    onClose: propTypes.func.isRequired,
-    largeImage: propTypes.string.isRequired,
-  };
-
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleClickBackdrop = event => {
+export default function ModalWindow({onClose, largeImage}) {
+  
+  useEffect(() => { 
+    const handleKeyDown = event => {
+      if (event.code === 'Escape') {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  },[onClose]);
+  
+  const handleClickBackdrop = event => {
     if (event.target === event.currentTarget) {
-      this.props.onClose();
+      onClose();
     }
   };
-
-  handleKeyDown = event => {
-    if (event.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-
-  render() {
-    return (
-      <Overlay onClick={this.handleClickBackdrop}>
-        <ModalImage>
-          <img src={this.props.largeImage} alt="" />
-        </ModalImage>
-      </Overlay>
-    );
-  }
+  
+  return (
+    <Overlay onClick={handleClickBackdrop}>
+      <ModalImage>
+        <img src={largeImage} alt="" />
+      </ModalImage>
+    </Overlay>
+  );
 }
 
-export default ModalWindow;
+ModalWindow.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  largeImage: PropTypes.string.isRequired,
+};
+
+// class ModalWindow extends Component {
+//   static propTypes = {
+//     onClose: propTypes.func.isRequired,
+//     largeImage: propTypes.string.isRequired,
+//   };
+
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
+  
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   handleClickBackdrop = event => {
+//     if (event.target === event.currentTarget) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   handleKeyDown = event => {
+//     if (event.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     return (
+//       <Overlay onClick={this.handleClickBackdrop}>
+//         <ModalImage>
+//           <img src={this.props.largeImage} alt="" />
+//         </ModalImage>
+//       </Overlay>
+//     );
+//   }
+// }
+
+// export default ModalWindow;
